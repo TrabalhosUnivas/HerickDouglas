7f08604ac398191306be16e1ec585c0f3c91c88a
TrabalhosUnivas <67248498+TrabalhosUnivas@users.noreply.github.com>
2020-06-25 20:07:23 -0300

    Update Questao3.java

--- src/br/edu/univas/Questao3.java
+++ src/br/edu/univas/Questao3.java
@@ -1,10 +1,25 @@
-package br.edu.univas;
+import java.util.Scanner;
 
 public class Questao3 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);	
+		System.out.println("Escreva um numero:");
+		
+		int x = sc.nextInt();
+		
+		if (x % 5 == 3) {
+			x = x + 2;	
+		}
+		
+		if (x % 5 == 4) {	
+			x = x + 1;		
+		}
+		
+		System.out.println(x);
+		sc.close();
+		
 	}
 
 }
