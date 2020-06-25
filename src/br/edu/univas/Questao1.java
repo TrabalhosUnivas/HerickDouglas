e152a76b9b8adf7c10d7e29917c22d021ef92993
TrabalhosUnivas <67248498+TrabalhosUnivas@users.noreply.github.com>
2020-06-25 20:02:17 -0300

    Update Questao1.java

--- src/br/edu/univas/Questao1.java
+++ src/br/edu/univas/Questao1.java
@@ -1,10 +1,34 @@
 package br.edu.univas;
 
+import java.util.Scanner;
+
 public class Questao1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		Scanner sc = new Scanner(System.in);
+		
+		float numero;
+		int cont = 0;
+		
+		do {
+			System.out.println("Escreva os numeros");
+			numero = sc.nextFloat();
+			
+			if  (numero > 0) {
+				cont++;
+			}
+			
+			if (numero == 0) {
+				
+				break;
+				
+			}
+		
+	} while (true);
+		
+		System.out.println("A total de numeros negativos digitados é: "+ cont);		
+		
+		sc.close();
+     }
 
 }
