fc073987da41df8a90f486657bfab7779286a286
TrabalhosUnivas <67248498+TrabalhosUnivas@users.noreply.github.com>
2020-06-25 16:37:33 -0300

    Add files via upload

---
+++ src/br/edu/univas/Questao2.java
@@ -0,0 +1,10 @@
+package br.edu.univas;
+
+public class Questao2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/br/edu/univas/Questao3.java
@@ -0,0 +1,10 @@
+package br.edu.univas;
+
+public class Questao3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
